6216c25aa1c622e4ab68e6be1035db7f6cab4eee
Lindsay Carr <lcarr@usgs.gov>
2017-10-18 14:41:26 -0500

    updates to conus maps based on feedback

--- scripts/conus_maps.R
+++ scripts/conus_maps.R
@@ -1,35 +1,38 @@
 ## Create map figures to use in algorithm fig
-## Takes approximately 5 minutes to run.
+## Takes approximately 10 minutes to run.
 ## Saves them in `images/`
 
 library(geoknife)
+library(lubridate)
 library(rgdal)
 library(rasterVis)
+library(sf)
 library(maps)
 library(ggmap)
 library(ggplot2)
 
+# setup stencil and knife
 stencil <- webgeom(geom = "sample:CONUS_states", attribute = "STATE", values = NA)
 knife <- webprocess("subset")
 
-dates <- list(time1 = c("2015-03-01", "2015-03-31"),
-              time2 = c("2015-05-01", "2015-05-31"),
-              time3 = c("2015-07-01", "2015-07-31"),
-              time4 = c("2015-09-01", "2015-09-30"),
-              time5 = c("2015-11-01", "2015-11-30"))
+# create timesteps
+starts <- seq(as.Date("2015-01-01"), as.Date("2015-08-01"), by="month")
+ends <- seq(as.Date("2015-02-01"), as.Date("2015-09-01"), by="month") - lubridate::days(1)
+dates_df <- data.frame(start_date = starts, end_date = ends, stringsAsFactors = FALSE)
 
-for(d in seq_along(dates)){
+for(r in seq(nrow(dates_df))){
   
-  dates_d <- dates[[d]]
+  start <- as.character(dates_df$start_date[r])
+  end <- as.character(dates_df$end_date[r])
 
   # execute geoknife job
   fabric <- webdata(url = 'https://cida.usgs.gov/thredds/dodsC/prism_v2', 
                     variables = "tmx", 
-                    times = dates_d)
+                    times = c(start, end))
   job <- geoknife(stencil, fabric, knife, wait=TRUE, OUTPUT_TYPE = "geotiff")
   
   # get raster file and location
-  fn <- paste0("tmx", names(dates[d]))
+  fn <- paste0("tmx", r, lubridate::month(start, label=TRUE))
   file <- download(job, destination = file.path(tempdir(), paste0(fn, '_data.zip')), overwrite=TRUE)
   unzip(file, exdir=file.path(tempdir(), fn))
   tiff.dir <- file.path(tempdir(), fn)
@@ -50,10 +53,11 @@ for(d in seq_along(dates)){
   
   map_plot <- gplot(temp_mask, maxpixels = 5e5) + 
     geom_tile(aes(fill = value), alpha=1) +
-    scale_fill_gradientn(colours=c("white", rev(rainbow(10))), na.value = "transparent", guide = FALSE) +
+    scale_fill_gradientn(colours=rev(rainbow(15)), na.value = "transparent",
+                         limits = c(-15, 50), guide = FALSE) +
     coord_equal() + 
     theme_minimal() + 
     theme(axis.text = element_blank(), axis.title = element_blank(), panel.grid = element_blank())
   
-  ggsave(paste0("images/", fn, ".png"), map_plot, width = 8, height = 7, units = "in")
+  ggsave(paste0("images/", fn, ".png"), map_plot, width = 8, height = 7, units = "in", bg="transparent")
 }
