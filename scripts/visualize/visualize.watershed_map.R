b84fc80e9c2848a020ce6135044d2c28fd06f430
Lindsay Carr <lcarr@usgs.gov>
2017-10-18 11:24:00 -0500

    actually add the figures so they end up in target

--- scripts/visualize/visualize.watershed_map.R
+++ scripts/visualize/visualize.watershed_map.R
@@ -1,10 +1,14 @@
 visualize.watershed_map <- function(viz){
+  library(ggplot2)
+  
   deps <- readDepends(viz)
-  region_sf <- deps[['region_sf']]
+  sp_poly <- deps[['sp_poly']]
   
-  library(sf)
+  watershed_map <- ggplot() + 
+    geom_polygon(data=sp_poly, aes(x=long, y=lat, group=group), fill="grey90", color="black") +
+    coord_fixed() +
+    theme_minimal() + 
+    theme(axis.title = element_blank(), panel.grid = element_blank(), axis.text = element_blank()) 
   
-  png(viz[['location']])
-  plot(region_sf, col="white")
-  dev.off()
+  ggsave(filename = viz[["location"]], plot = watershed_map)
 }
