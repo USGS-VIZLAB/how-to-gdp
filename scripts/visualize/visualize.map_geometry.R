ce4f3d358271e65e6c2b57e2e83191df39c3655c
Lindsay Carr <lcarr@usgs.gov>
2017-10-31 13:52:39 -0500

    use sp:: rather than library(sp)

--- scripts/visualize/visualize.map_geometry.R
+++ scripts/visualize/visualize.map_geometry.R
@@ -4,10 +4,8 @@ visualize.map_geometry <- function(viz){
   geom_feature <- deps[['geom_feature']]
   geom_context <- deps[['geom_context']]
   
-  library(sp)
-  
   png(viz[['location']])
-  plot(geom_context, col="grey")
-  plot(geom_feature, col="lightyellow", add=TRUE)
+  sp::plot(geom_context, col="grey")
+  sp::plot(geom_feature, col="lightyellow", add=TRUE)
   dev.off()
 }
