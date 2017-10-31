06b2bb3697c12c264570b0ec84e3141c2e7201b0
Lindsay Carr <lcarr@usgs.gov>
2017-10-31 13:22:42 -0500

    use maptools::elide to transpose

--- scripts/fetch/fetch.geom_transpose.R
+++ scripts/fetch/fetch.geom_transpose.R
@@ -1,7 +1,7 @@
 fetch.geom_transpose <- function(viz){
   deps <- readDepends(viz)
   
-  sp_poly_transpose <- raster::shift(deps[["geom_sp"]], y = viz[["vertical_trans_va"]])
+  sp_poly_transpose <- maptools::elide(deps[["geom_sp"]], shift = c(0, viz[["vertical_trans_va"]]))
   
   saveRDS(sp_poly_transpose, viz[['location']])
 }
