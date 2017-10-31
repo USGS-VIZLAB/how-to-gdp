4737f693fee510d85aa3a76df7c02921a36cb5ce
Lindsay Carr <lcarr@usgs.gov>
2017-10-31 09:43:05 -0500

    precip bins cols and breaks working

--- scripts/process/process.utils.R
+++ scripts/process/process.utils.R
@@ -1,27 +1,3 @@
-create_colors <- function(n, palette = "Blues"){
-  RColorBrewer::brewer.pal(n, palette)
-}
-
-create_breaks <- function(nbins, stepsize){
-  seq(from = 0, to = stepsize * (nbins - 1), length.out = nbins)
-}
-
-#' calculate cumulative precipitation and store as `precipVal`
-#' 
-#' this function assumes column names `DateTime`, 
-#' `id` (location of precip summarization), and `precipVal`
-#' 
-calc_cumulative_precip <- function(precip_df){
-  `%>%` <- magrittr::`%>%`
-  
-  precip <- dplyr::group_by(precip_df, id) %>% 
-    dplyr::mutate(precipVal = precipVal/25.4) %>% #convert mm to inches
-    dplyr::mutate(summ = cumsum(precipVal)) %>% 
-    dplyr::select(DateTime, id, precipVal = summ)
-  
-  return(precip)
-}
-
 #' add a column to precip data based on the color bin  the precip
 #' value corresponds to
 #' 
