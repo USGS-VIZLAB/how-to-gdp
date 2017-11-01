eb5a57669fe36f4f4a4d07fbaee49a90fd26cb37
Lindsay Carr <lcarr@usgs.gov>
2017-11-01 16:11:57 -0500

    make ids for each sp object unique

--- scripts/fetch/fetch.geom_shp.R
+++ scripts/fetch/fetch.geom_shp.R
@@ -7,7 +7,7 @@ fetch.geom_shp <- function(viz){
   sp_poly <- rgdal::readOGR(dsn = viz[["filepath"]])
   
   # merge multiple polygons into one large polygon
-  sp_poly@data <- dplyr::mutate(sp_poly@data, polygon = "one_polygon")
+  sp_poly@data <- dplyr::mutate(sp_poly@data, polygon = viz[["id"]])
   sp_poly_dissolve <- maptools::unionSpatialPolygons(sp_poly, IDs = sp_poly@data$polygon)
   
   # reproject the sp object
--- scripts/fetch/fetch.geom_transpose.R
+++ scripts/fetch/fetch.geom_transpose.R
@@ -4,6 +4,12 @@ fetch.geom_transpose <- function(viz){
   sp_poly_transpose <- maptools::elide(deps[["geom_sp"]], shift = c(0, viz[["vertical_trans_va"]]))
   sp::proj4string(sp_poly_transpose) <- sp::proj4string(deps[["geom_sp"]])
   
+  # create unique ids for the new transposed polygons
+  # there should only be one polygon at this point:
+  stopifnot(length(sp_poly_transpose@plotOrder) == 1)
+  new_id <- paste0(slot(sp_poly_transpose@polygons[[1]], name = "ID"), "_transposed")
+  slot(sp_poly_transpose@polygons[[1]], name = "ID") <- new_id
+  
   saveRDS(sp_poly_transpose, viz[['location']])
 }
 
