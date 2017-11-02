52ec4ade005baa480a9324227de9a0158885f977
Lindsay Carr <lcarr@usgs.gov>
2017-11-02 16:01:22 -0500

    update process timestep so it actually returns data

--- scripts/process/process.precip_timestep.R
+++ scripts/process/process.precip_timestep.R
@@ -3,8 +3,9 @@ process.precip_timestep <- function(viz){
   deps <- readDepends(viz)
   precip_data <- deps[['precip_data']]
   timestep <- deps[["timestep"]][["date"]]
+  timezone <- deps[["timestep"]][["tz"]]
   
-  precip_data_ts <- dplyr::filter(precip_data, DateTime == timestep)
+  precip_data_ts <- dplyr::filter(precip_data, DateTime == as.POSIXct(timestep, tz=timezone))
   
   saveRDS(precip_data_ts, viz[['location']])
 }
