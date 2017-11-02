65aa599f8fb0a9f536a2d96c60b3e4b17ec607ee
Lindsay Carr <lcarr@usgs.gov>
2017-11-02 08:43:46 -0500

    add a legend + update limits

--- scripts/visualize/visualize.map_precip.R
+++ scripts/visualize/visualize.map_precip.R
@@ -14,14 +14,25 @@ visualize.map_precip <- function(viz){
   png(viz[['location']])
   
   if(!is.null(geom_sp_orig)){
-    sp::plot(geom_sp_orig, col="lightgrey")
+    
+    # calculate the center of both polygons (combine them first)
+    # so that appropriate limits can be used for the plot
+    geom_sp_combined <- maptools::spRbind(geom_sp, geom_sp_orig)
+    geom_sp_bbox <- sp::bbox(geom_sp_combined)
+    
+    sp::plot(geom_sp_orig, col="lightgrey", border = NA, 
+             xlim = geom_sp_bbox["x",], ylim = geom_sp_bbox["y",])
+    
     add_to_map <- TRUE
   } else {
     add_to_map <- FALSE
   }
   
-  sp::plot(geom_sp, col="darkgrey", add=add_to_map)
+
   sp::plot(geom_sp, col = precip_col, border = NA, add=add_to_map)
+  legend("bottom", legend = paste("<", round(precip_breaks[-1], 2)),
+         fill = precip_colors, border = NA, ncol = 5, box.col = NA,
+         cex = 0.8, x.intersp = 0.5, bg = "lightgrey", inset = -0.3, xpd = TRUE)
   
   dev.off()
   
