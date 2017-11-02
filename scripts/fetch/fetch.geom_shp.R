2749bec904b9a3faa0d0d2b47aa6f9a6b11af290
Lindsay Carr <lcarr@usgs.gov>
2017-11-02 16:00:40 -0500

    fix projection so shp is in correct place!

--- scripts/fetch/fetch.geom_shp.R
+++ scripts/fetch/fetch.geom_shp.R
@@ -1,17 +1,20 @@
 fetch.geom_shp <- function(viz){
   
+  shp_crs <- viz[["shp_crs"]]
+  
   deps <- readDepends(viz)
-  crs <- deps[["crs"]][["crs_str"]]
+  transform_crs <- deps[["transform_crs"]][["crs_str"]]
   
   # read in the shapefile
   sp_poly <- rgdal::readOGR(dsn = viz[["filepath"]])
+  sp_poly@proj4string <- sp::CRS(shp_crs)
   
   # merge multiple polygons into one large polygon
   sp_poly@data <- dplyr::mutate(sp_poly@data, polygon = viz[["id"]])
   sp_poly_dissolve <- maptools::unionSpatialPolygons(sp_poly, IDs = sp_poly@data$polygon)
   
   # reproject the sp object
-  sp_poly_transf <- sp::spTransform(sp_poly_dissolve, CRSobj = crs)
+  sp_poly_transf <- sp::spTransform(sp_poly_dissolve, CRSobj = transform_crs)
   
   saveRDS(sp_poly_transf, viz[['location']])
 }
