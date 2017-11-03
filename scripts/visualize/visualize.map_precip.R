7ae2eeeb55236074ed6c26d1e44d3da110af2bd5
Lindsay Carr <lcarr@usgs.gov>
2017-11-03 17:11:53 -0500

    expand bbox to fix splicing thing

--- scripts/visualize/visualize.map_precip.R
+++ scripts/visualize/visualize.map_precip.R
@@ -27,16 +27,28 @@ visualize.map_precip <- function(viz){
     
     geom_combined <- maptools::spRbind(geom_sp, geom_sp_orig)
     geom_center <- rgeos::gCentroid(geom_combined)@coords
+    geom_bbox <- sp::bbox(geom_combined)
     
   } else {
     geom_center <- rgeos::gCentroid(geom_sp)@coords
+    geom_bbox <- sp::bbox(geom_sp)
   }
- 
+  
   # create basemap
   basemap_data <- ggmap::get_map(location = c(lon = geom_center[1], lat = geom_center[2]),
                                  color = "bw", maptype = "toner", zoom = 10)
+  
+  # expand bbox if needed
+  basemap_bbox <- attr(basemap_data, "bb")
+  bottom <- ifelse(basemap_bbox[["ll.lat"]] > geom_bbox[2, 1], geom_bbox[2, 1], basemap_bbox[["ll.lat"]])
+  top <- ifelse(basemap_bbox[["ur.lat"]] < geom_bbox[2, 2], geom_bbox[2, 2], basemap_bbox[["ur.lat"]])
+  left <- ifelse(basemap_bbox[["ll.lon"]] > geom_bbox[1, 1], geom_bbox[1, 1], basemap_bbox[["ll.lon"]])
+  right <- ifelse(basemap_bbox[["ur.lon"]] < geom_bbox[1, 2], geom_bbox[1, 2], basemap_bbox[["ur.lon"]])
+  attr(basemap_data, "bb") <- data.frame(ll.lat = bottom, ll.lon = left, ur.lat = top, ur.lon = right)
+  
+  # actually plot map
   basemap <- ggmap::ggmap(basemap_data) + 
-    theme(axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank())
+    theme(axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank()) 
   
   map_geometry <- basemap + 
     geom_polygon(data = geom_sp_df, aes(x = long, y = lat, group = group),
