6429ce82339037b4a73977c9b58bea1cf5116a72
Lindsay Carr <lcarr@usgs.gov>
2017-11-27 11:43:42 -0600

    change geometry fig to use ggplot2 & new basemap

--- scripts/visualize/visualize.map_geometry.R
+++ scripts/visualize/visualize.map_geometry.R
@@ -1,11 +1,24 @@
 visualize.map_geometry <- function(viz){
   
+  library(ggplot2)
+  
   deps <- readDepends(viz)
   geom_feature <- deps[['geom_feature']]
-  geom_context <- deps[['geom_context']]
+  basemap <- deps[["basemap"]]
+  
+  # prep sp data for ggplot mapping
+  geom_feature_sp_df <- ggplot2::fortify(geom_feature) 
+  
+  map_plot <- basemap +
+    geom_polygon(data = geom_feature_sp_df, 
+                 ggplot2::aes(x=long, y=lat, group=group),
+                 fill = "forestgreen", alpha = 0.2,
+                 color = "forestgreen") +
+    ggplot2::theme(axis.text = ggplot2::element_blank(), 
+                   axis.title = ggplot2::element_blank(), 
+                   panel.grid = ggplot2::element_blank())
   
   png(viz[['location']])
-  sp::plot(geom_context, col="grey")
-  sp::plot(geom_feature, col="lightyellow", add=TRUE)
+  print(map_plot)
   dev.off()
 }
