637a3a25161411ff8bbe8cb1946bafa3c3428646
Lindsay Carr <lcarr@usgs.gov>
2017-11-27 13:41:52 -0600

    remove commented code

--- scripts/visualize/visualize.create_basemap.R
+++ scripts/visualize/visualize.create_basemap.R
@@ -12,14 +12,6 @@ visualize.create_basemap <- function(viz){
   # create basemap
   basemap_data <- ggmap::get_map(location = geom_bbox, color = "bw", maptype = "toner")
   
-  # expand bbox if needed
-  # basemap_bbox <- attr(basemap_data, "bb")
-  # bottom <- ifelse(basemap_bbox[["ll.lat"]] > geom_bbox[2, 1], geom_bbox[2, 1], basemap_bbox[["ll.lat"]])
-  # top <- ifelse(basemap_bbox[["ur.lat"]] < geom_bbox[2, 2], geom_bbox[2, 2], basemap_bbox[["ur.lat"]])
-  # left <- ifelse(basemap_bbox[["ll.lon"]] > geom_bbox[1, 1], geom_bbox[1, 1], basemap_bbox[["ll.lon"]])
-  # right <- ifelse(basemap_bbox[["ur.lon"]] < geom_bbox[1, 2], geom_bbox[1, 2], basemap_bbox[["ur.lon"]])
-  # attr(basemap_data, "bb") <- data.frame(ll.lat = bottom, ll.lon = left, ur.lat = top, ur.lon = right)
-  
   # actually plot map
   basemap <- ggmap::ggmap(basemap_data) + 
     theme(axis.title = element_blank(), 
